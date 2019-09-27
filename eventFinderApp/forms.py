a679c5c027443214dacda21847812275f9917308
“jojo-baker” <jo.alice.baker@gmail.com>
2019-09-27 20:43:38 +0800

    Date time splitter working with add event form

--- eventFinderApp/forms.py
+++ eventFinderApp/forms.py
@@ -1,16 +1,14 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, SplitDateTimeField
 from .models import Event, Category, Account
 from django.contrib.admin import widgets
 
 class EventForm(ModelForm):
+    start_time = SplitDateTimeField(widget=widgets.AdminSplitDateTime())
+    end_time = SplitDateTimeField(widget=widgets.AdminSplitDateTime())
+
     class Meta:
         model = Event
         exclude = ['host']
-        widgets = {
-            'start_time': widgets.AdminSplitDateTime,
-            'end_time': widgets.AdminSplitDateTime,
-        }
-
 
 
 class AccountForm(ModelForm):
